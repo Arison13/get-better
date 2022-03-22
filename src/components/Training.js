f063f5efbee93abbb23f61c59d27826a1df00add
Arison13 <arisonarias@gmail.com>
2022-03-22 18:47:04 -0400

    filter and search funcionality done

--- src/components/Training.js
+++ src/components/Training.js
@@ -4,37 +4,37 @@ import "../styles/Training.css";
 
 export default function Training() {
   const [data, setData] = useState(exercises);
-  // const filteredExercise = exercises.filter(e => e.muscule == "Back") console.log(filteredExercise)
-  //To Filter exercise based on input
+  const [search, setSearch] = useState("");
 
-  const onChange = (e) => {
-    console.log(e.target.name, e.target.value)
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const filteredExercises = exercises.filter(e => {
+      if(search === "All" || search === "all"){
+        return data;
+      }
+      else{ return e.muscule === search;}
+    })
+    setSearch("")
+    setData(filteredExercises)
   }
-  
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   const filteredExercises = exercises.filter(e =>{
-  //   return e.muscule === "Back"
-  //   })
-  //   setData(filteredExercises)
-  // }
   return (
     <div className='training'>
       <div className="search">
-        <form> 
+        <form onSubmit={onSubmit}> 
           <label>Filter By Muscule:</label>
             <input className='search-input' 
             type='text' 
             name='search'
-            // value=""
-            onChange={onChange}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
             placeholder='eg: Back, Chest, Shoulders' />
             <button className='submit-btn'>Search</button>
           </form>
+          {/* <button className='submit-btn'>Reset Filter</button> */}
       </div>
       <div className='exercise-group'>
         {
-          exercises.map(e => {
+          data.map(e => {
             return <ExerciseBuilder exercise={e} key={e.name}/>
           })
         }
@@ -47,8 +47,8 @@ export default function Training() {
     return (
       <div className='exercise-card'>
           {/* optional add image */}
-          <h4>{exercise.name}</h4>
-          <p>Working Muscule(s): {exercise.muscule}</p>
+          <h4>{exercise.muscule}</h4>
+          <p>Exercise: {exercise.name} </p>
           <p>Reps:{exercise.reps}</p>
           <p>Weight:{exercise.weight}</p>
           <p>Sets:{exercise.sets}</p>
