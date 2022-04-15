96fcd9330aa98c872537ec59af418c9036dbf990
Arison13 <arisonarias@gmail.com>
2022-04-15 12:34:26 -0400

    most of the basic styles for modal done

--- src/components/AddExercise.js
+++ src/components/AddExercise.js
@@ -1,24 +1,42 @@
 import React, {useState} from 'react'
 import '../styles/AddExercise.css';
+import {FiX } from "react-icons/fi";
 
 export default function AddExercise({open, setOpen}) {
 
   return (
     <> 
-    <dialog open={open}>
+    <dialog className = "dialog" open={open}>
+        <header className='modal-header'> 
         <h3>AddExercise </h3>
-        <p>Tring to add modal</p>
-        <form>
-            <label> Enter name </label>
+        <FiX onClick={()=> setOpen(false)}/>
+        </header>
+        <form className='add-form'>
+            <div className='question'> 
+                <label> Enter muscule targeted </label>
                 <input type='text' placeholder="trying stuff"/>
-            <label> Enter name </label>
+            </div>
+            <div className='question'> 
+                <label> Enter exercise name </label>
                 <input type='text' placeholder="trying stuff"/>
-            <label> Enter name </label>
+            </div>
+            <div className='question'> 
+                <label> Enter numer of reps </label>
                 <input type='text' placeholder="trying stuff"/>
-            <label> Enter name </label>
+            </div>
+            
+            <div className='question'> 
+                <label> Enter number of sets </label>
                 <input type='text' placeholder="trying stuff"/>
+            </div>
+            <div className='question'> 
+                <label> Enter any tips </label>
+                <input type='text' placeholder="trying stuff"/>
+            </div>
         </form>
-        <button onClick={()=> setOpen(false)}> Submit </button>
+        <div className='btn-div'> 
+            <button className='exercise-submit' onClick={()=> setOpen(false)}> Submit </button>
+        </div> 
     </dialog>
     </>
   )
