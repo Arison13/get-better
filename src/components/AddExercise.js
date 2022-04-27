406bb7a2ead5484fe2d45e93d8e641fd7c7b43ce
Arison13 <arisonarias@gmail.com>
2022-04-27 11:47:24 -0400

    added input handling to add exercise form

--- src/components/AddExercise.js
+++ src/components/AddExercise.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/AddExercise.css';
-import {FiX } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
+
+const initialFormValues = {
+    muscule:"",
+    exercise:"",
+    reps:"",
+    sets:"",
+    tips:""
+  };
 export default function AddExercise({open, setOpen}) {
+    const [formValues, setFormValues] = useState(initialFormValues)
+
+    const handleOnChange = ev => {
+        setFormValues({
+            ...formValues, 
+            [ev.target.name]:ev.target.value
+        })
+    }
+
+    const onSubmit = ev => {
+        ev.preventDefault();
+        console.log(formValues)
+        setOpen(false)  
+    }
 
   return (
     <> 
@@ -11,32 +33,34 @@ export default function AddExercise({open, setOpen}) {
             <h3>New Exercise </h3>
             <FiX onClick={()=> setOpen(false)}/>
         </header>
-        <form className='add-form' >
+        <form className='add-form' onSubmit={onSubmit} >
             <div className='question'> 
                 <label> Enter muscule targeted </label>
-                <input type='text' placeholder="trying stuff"/>
+                <input value={formValues.muscule} name="muscule" type='text' onChange={handleOnChange} placeholder="Enter muscule"/>
             </div>
             <div className='question'> 
                 <label> Enter exercise name </label>
-                <input type='text' placeholder="trying stuff"/>
+                <input value={formValues.exercise} name='exercise' type='text' onChange={handleOnChange} placeholder="Enter exercise"/>
             </div>
             <div className='question'> 
                 <label> Enter numer of reps </label>
-                <input type='text' placeholder="trying stuff"/>
+                <input value={formValues.reps} name='reps' type='text' onChange={handleOnChange} placeholder="Enter reps"/>
             </div>
             
             <div className='question'> 
                 <label> Enter number of sets </label>
-                <input type='text' placeholder="trying stuff"/>
+                <input value={formValues.sets} name='sets' type='text' onChange={handleOnChange} placeholder="Enter sets"/>
             </div>
             <div className='question'> 
                 <label> Enter any tips </label>
-                <input type='text' placeholder="trying stuff"/>
+                <input value={formValues.tips} name='tips' type='text' onChange={handleOnChange} placeholder="Enter tips"/>
             </div>
+            
+            <div className='btn-div'> 
+                <button className='exercise-submit' onSubmit={onSubmit} >Add Exercise!</button>
+            </div> 
         </form>
-        <div className='btn-div'> 
-            <button className='exercise-submit' onClick={()=> setOpen(false)}>Add Exercise!</button>
-        </div> 
+        
     </dialog>
     </>
   )
